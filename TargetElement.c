c93559b7530aba7738d4bb4b6dc5498feeaa12cf
janvimulani <sq.krish13@gmail.com>
2024-08-07 18:19:39 +0530

    Target Element find

--- TargetElement.c
+++ TargetElement.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
 
-void Sum1(int arr[], int size, int target, int Index, int Sum, int Combination[], int Length) {
-    if (Sum == target) {
+void Sum1(int arr[], int size, int target, int Index, int Sum, int Combination[], int Length)
+{
+    if (Sum == target)
+    {
         printf("Elements at positions: ");
-        for (int i = 0; i < Length; i++) {
+        for (int i = 0; i < Length; i++)
+        {
             printf("%d ", Combination[i]);
         }
         printf("the sum %d\n", target);
-        
+
         return;
     }
 
-    if (Sum > target || Index == size) {
+    if (Sum > target || Index == size)
+    {
         return;
     }
     Combination[Length] = Index;
     Sum1(arr, size, target, Index + 1, Sum + arr[Index], Combination, Length + 1);
 
-    
     Sum1(arr, size, target, Index + 1, Sum, Combination, Length);
 }
 
-int main() {
+int main()
+{
     int size;
     printf("Enter the number of elements in the array: ");
     scanf("%d", &size);
@@ -30,7 +34,8 @@ int main() {
     int arr[size];
 
     printf("Enter the elements of the array:\n");
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         scanf("%d", &arr[i]);
     }
 
@@ -39,11 +44,12 @@ int main() {
     scanf("%d", &target);
 
     int Combination[size];
-     int found = 0; 
+    int found = 0;
 
-    Sum1(arr, size, target, 0, 0, Combination, 0 );
+    Sum1(arr, size, target, 0, 0, Combination, 0);
 
-    if (!found) {
+    if (!found)
+    {
         printf("No combination found that sums up to %d\n", target);
     }
 
